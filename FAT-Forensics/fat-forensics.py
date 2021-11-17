Fix Score calculation for regression target, update Y analysis

FAT-Forensics/fat-forensics.py
@@ -1,6 +1,7 @@
 # Author: Kacper Sokol <k.sokol@bristol.ac.uk>
 # License: new BSD
 import json
+import os
 import time
 
 import fatf.transparency.predictions.counterfactuals as fatf_cf
@@ -17,7 +18,7 @@ from common.Target import Target
 #     standard with ``,`` used as the delimiter. The first row of the file must
 #     be a header formatted as follows:
 #     ``n_samples,n_features,class_name_1,class_name_2,...``
-def format_csv_fatf(file_name):
+def load_csv_fatf(file_name):
     dataset = pd.read_csv(csv_path)
     shapes = [dataset.shape[0], dataset.shape[1] - 1]
     target_column_name = dataset.columns[len(dataset.columns) - 1]
@@ -34,12 +35,15 @@ def format_csv_fatf(file_name):
         while i < len(data):
             write_obj.write(', '.join(str(e) for e in data[i]) + ', ' + target[i] + '\n')
             i += 1
-    # os.remove(file_name)
+    try:
+        dataset = fatf_datasets.load_data(modified_csv)
+    finally:
+        os.remove(modified_csv)
+    return dataset
 
 
-def calculate_scores(counterfactuals, predictions, target, X, Y):
+def calculate_scores(counterfactuals, predictions, target, data_analyzer):
     print("Generated counterfactuals {}".format(len(counterfactuals)))
-    data_analyzer = DataAnalyzer(X, Y)
     score_calculator = ScoreCalculator(initial_instance, initial_prediction, target, data_analyzer)
     print("Scores")
     scores = []
@@ -51,10 +55,13 @@ def calculate_scores(counterfactuals, predictions, target, X, Y):
 
 
 csv_path = "datasets/diabetes.csv"
-target = Target("classification", "tested_negative")
-format_csv_fatf(csv_path)
+target = Target("classification", "class", "tested_negative")
+initial_instance_index = 0
 
-dataset = fatf_datasets.load_data(csv_path + ".mod")
+dataset = load_csv_fatf(csv_path)
+dataframe = pd.read_csv(csv_path)
+data_analyzer = DataAnalyzer(dataframe, target)
+
 X = np.array(dataset['data'])
 Y = dataset['target']
 
@@ -71,7 +78,6 @@ cf_explainer = fatf_cf.CounterfactualExplainer(
     default_numerical_step_size=0.1)
 
 # Select a data point to be explained
-initial_instance_index = 0
 initial_instance = X[initial_instance_index]
 initial_prediction = Y[initial_instance_index]
 
@@ -81,7 +87,7 @@ explanation_tuple = cf_explainer.explain_instance(initial_instance)
 total_time = time.process_time() - t
 counterfactuals, distances, predictions = explanation_tuple
 
-scores = calculate_scores(counterfactuals, predictions, target, X, Y)
+scores = calculate_scores(counterfactuals, predictions, target, data_analyzer)
 
 clf_predictions = clf.predict(counterfactuals)
 print(predictions)
@@ -94,8 +100,7 @@ output = {
     "target_value": target.target_value(),
     "total_time": total_time,
     "counterfactuals": counterfactuals.tolist(),
-    "predictions": clf_predictions.tolist(),
-    "scores": scores
+    "predictions": clf_predictions.tolist()
 }
 output_filename = "fatf_output.json"
 with open(output_filename, 'w') as outfile:

bcgxai/run.py
@@ -1,5 +1,6 @@
 import json
 
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
@@ -8,20 +9,36 @@ import time_measurement
 from common.DataAnalyzer import DataAnalyzer
 from common.Target import Target
 
-dataset_filename = "datasets/diabetes.csv"
-target = Target(target_type="classification", target_value="tested_negative")
+# dataset_filename = "datasets/diabetes.csv"
+# target = Target(target_type="classification", target_feature="class", target_value="tested_negative")
+# initial_instance_index = 0
+
+# dataset_filename = "datasets/kc2.csv"
+# target = Target(target_type="classification", target_feature="problems", target_value="no")
+# initial_instance_index = 4
+
+# dataset_filename = "datasets/pd_speech_features.csv"
+# target = Target(target_type="classification", target_feature="class", target_value=0)
+# initial_instance_index = 0
+
+dataset_filename = "datasets/house_sales.csv"
+target = Target(target_type="regression", target_feature="price", target_value="decrease")
+initial_instance_index = 0
+cat_features = ["waterfront", "date_year"]
 
 df = pd.read_csv(dataset_filename)
-X = df.values[:, :-1]
-Y = df.values[:, -1]
+data_analyzer = DataAnalyzer(df, target=target)
+X, Y = data_analyzer.split_dataset()
 
-data_analyzer = DataAnalyzer(X, Y, target=target)
-
-initial_instance_index = 0
 initial_instance = X[initial_instance_index]
 initial_prediction = Y[initial_instance_index]
 model = RandomForestClassifier()  # pluggable model that we train to explain.
-model.fit(X[initial_instance_index + 1:], Y[initial_instance_index + 1:])
+np.delete(X, initial_instance_index)
+np.delete(Y, initial_instance_index)
+print("Training model to explain")
+# model.fit(X, Y)
+model.fit(X[:200, :], Y[:200])
+print("Finished training")
 
 instancesInfo = bcg_xai.run(initial_instance, initial_prediction, target, data_analyzer, model)
 
@@ -34,6 +51,7 @@ output = {
     "initial_prediction": initial_prediction,
     "target_type": target.target_type(),
     "target_value": target.target_value(),
+    "target_feature": target.target_feature(),
     "total_time": time_measurement.total_time,
     "time_to_first_solution": time_measurement.time_to_first_solution,
     "time_to_best_solution": time_measurement.time_to_best_solution,

common/DataAnalyzer.py
@@ -4,30 +4,34 @@ from common.Target import Target
 
 
 class DataAnalyzer:
-    def __init__(self, X, Y, target, cat_features=None, feature_weights=None):
-        self._analyze_x(X, cat_features, feature_weights)
-        self._analyze_y(Y, target)
+    def __init__(self, dataframe, target, cat_features=None, feature_weights=None):
+        if feature_weights is None:
+            feature_weights = {}
+        if cat_features is None:
+            cat_features = []
+        target_feature = target.target_feature()
+        assert target_feature in dataframe.columns
+        self._Y = dataframe[[target_feature]].values.ravel()
+        dataframe.drop([target_feature], axis=1, inplace=True)
+        self._X = dataframe.values
 
-    def _analyze_x(self, X, cat_features, feature_weights):
-        self._features_count = X.shape[1]
-        self._X_min_values = np.min(X, axis=0)
-        self._X_max_values = np.max(X, axis=0)
+        self._analyze_x(dataframe.columns, cat_features, feature_weights)
+        self._analyze_y(target)
 
-        # check if feature weights info have been provided, if not, infer it
-        if np.array(feature_weights).any():
-            self._feature_weights = feature_weights
-        else:
-            self._feature_weights = np.ones(self._features_count)
+    def split_dataset(self):
+        return self._X, self._Y
 
-        # check if categorical features info have been provided, if not, infer it
-        if np.array(cat_features).any():
-            self._categorical_features = cat_features
-        else:
-            self._categorical_features = np.zeros(self._features_count, dtype=bool)
-            for col in range(self._features_count):
-                if not np.issubdtype(type(X[0, col]), np.number):
-                    self._categorical_features[col] = True
+    def _analyze_x(self, feature_names, cat_features, feature_weights):
+        self._features_count = self._X.shape[1]
+        self._X_min_values = np.min(self._X, axis=0)
+        self._X_max_values = np.max(self._X, axis=0)
 
+        self._feature_weights = [feature_weights[f] if f in feature_weights else 1 for f in feature_names]
+        self._categorical_features = [True if f in cat_features else False for f in feature_names]
+        # perform additional check for strings and treat them as categories as well
+        for idx in range(len(feature_names)):
+            if not np.issubdtype(type(self._X[0, idx]), np.number):
+                self._categorical_features[idx] = True
         self._numerical_features = np.logical_not(self._categorical_features)
 
         # create ranges for features, numerical and categorical
@@ -51,16 +55,16 @@ class DataAnalyzer:
     def feature_weights(self):
         return self._feature_weights
 
-    def _analyze_y(self, Y, target):
+    def _analyze_y(self, target):
         if target.target_type() is not Target.TYPE_CLASSIFICATION:
-            self._Y_min_values = np.min(Y, axis=0)
-            self._Y_max_values = np.max(Y, axis=0)
+            self._Y_min = np.min(self._Y, axis=0)
+            self._Y_max = np.max(self._Y, axis=0)
         else:
-            self._Y_min_values = None
-            self._Y_max_values = None
+            self._Y_min = None
+            self._Y_max = None
 
     def prediction_min_value(self):
-        return self._Y_min_values
+        return self._Y_min
 
     def prediction_max_value(self):
-        return self._Y_max_values
+        return self._Y_max

common/ScoreCalculator.py
@@ -5,16 +5,18 @@ from common.Target import Target
 ZERO_VALUE = 0.1
 
 
-def score_y_away_from_target(min_value, predictions, max_value):
-    predictions_diff = np.abs(min_value - predictions)
-    total_diff = np.abs(min_value - max_value)
-    return (1 - np.divide(predictions_diff, total_diff)) * ZERO_VALUE
-
-
-def score_y_reaching_target(min_value, predictions, max_value):
-    predictions_diff = np.abs(predictions - min_value)
+def score_y_away_from_target(min_value, initial_prediction, predictions, max_value):
+    predictions_diff = np.abs(initial_prediction - predictions)
     total_diff = np.abs(max_value - min_value)
-    return np.divide(predictions_diff, total_diff)
+    result = (1 - np.divide(predictions_diff, total_diff)) * ZERO_VALUE
+    return result
+
+
+def score_y_reaching_target(min_value, initial_prediction, predictions, max_value):
+    predictions_diff = np.abs(predictions - initial_prediction)
+    total_diff = np.abs(max_value - min_value)
+    result = np.divide(predictions_diff, total_diff)
+    return result
 
 
 class ScoreCalculator:
@@ -30,7 +32,7 @@ class ScoreCalculator:
         # calculate closeness of the potential counterfactual to the initial instance.
         score_x = self.score_x(self._initial_instance, instances)
         score_y = self.score_y(predictions)
-        return score_x * score_y
+        return np.round(score_x * score_y, 3)
 
     # returns np.array of gower distances for each instance against the initial one
     def gower_distance(self, origin_instance, instances):
@@ -51,7 +53,7 @@ class ScoreCalculator:
             partial_gowers[:, feature_idx] = np.multiply(ij, feature_weight)
 
         sum_gowers = np.sum(partial_gowers, axis=1)
-        gowers = np.divide(sum_gowers, self._data_analyzer.feature_weights().sum())
+        gowers = np.divide(sum_gowers, sum(self._data_analyzer.feature_weights()))
 
         return gowers
 
@@ -61,7 +63,7 @@ class ScoreCalculator:
     def score_y(self, predictions_to_calculate):
         if self._target.target_type() == Target.TYPE_CLASSIFICATION:
             return self.calculate_classification_score_y(predictions_to_calculate)
-        if self._target.target_type == Target.TYPE_REGRESSION:
+        if self._target.target_type() == Target.TYPE_REGRESSION:
             return self.calculate_regression_score_y(predictions_to_calculate)
         if self._target.target_type() == Target.TYPE_RANGE:
             return self.calculate_ranged_score_y(predictions_to_calculate)
@@ -71,22 +73,22 @@ class ScoreCalculator:
 
     def calculate_regression_score_y(self, predictions):
         # calculate for each prediction if is greater or lower than the initial one
-        increase_index = self._initial_instance >= predictions
-        decrease_index = not increase_index
-        max_target_value = self._data_analyzer.prediction_max_value()
-        min_target_value = self._data_analyzer.prediction_min_value()
+        increase_index = predictions >= self._initial_prediction
+        decrease_index = np.invert(increase_index)
+        max_prediction = self._data_analyzer.prediction_max_value()
+        min_prediction = self._data_analyzer.prediction_min_value()
 
-        scores_y = np.array([], dtype=np.float).reshape(predictions.shape[0])
+        scores_y = np.array([0] * len(predictions), dtype=np.float)
         if self._target.target_value() == Target.REGRESSION_INCREASE:
             scores_y[increase_index] = score_y_reaching_target(
-                self._initial_instance, predictions[increase_index], max_target_value)
+                self._initial_prediction, self._initial_prediction, predictions[increase_index], max_prediction)
             scores_y[decrease_index] = score_y_away_from_target(
-                self._initial_prediction, predictions[decrease_index], min_target_value)
+                min_prediction, self._initial_prediction, predictions[decrease_index], self._initial_prediction)
         elif self._target.target_value() == Target.REGRESSION_DECREASE:
             scores_y[decrease_index] = score_y_reaching_target(
-                self._initial_instance, predictions[decrease_index], min_target_value)
+                min_prediction, self._initial_prediction, predictions[decrease_index], self._initial_prediction)
             scores_y[increase_index] = score_y_away_from_target(
-                self._initial_prediction, predictions[increase_index], max_target_value)
+                self._initial_prediction, self._initial_prediction, predictions[increase_index], max_prediction)
         return scores_y
 
     def calculate_ranged_score_y(self, predictions):