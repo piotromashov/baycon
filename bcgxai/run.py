c2983996b0f8d748e6bdaeeefb8fc866c699178b
Piotr Romashov <promachov@gmail.com>
2021-11-26 20:53:20 +0100

    Add model "caching"

--- bcgxai/run.py
+++ bcgxai/run.py
@@ -23,14 +23,22 @@ class ModelWrapper:
         return class_prediction == target.target_value()
 
 
-def custom_model(dataset_filename, X, Y):
-    if dataset_filename != 'datasets/mnist.csv':
+def prepare_model(dataset, X, Y):
+    import pickle
+    if dataset != 'mnist':
         from sklearn.ensemble import RandomForestClassifier
         model = RandomForestClassifier()
         # model = KNeighborsClassifier()
-        print("Training model to explain")
-        model.fit(X, Y)
-        print("Finished training")
+        model_filename = "{0}_{1}.sav".format(type(model).__name__, dataset)
+        try:
+            print("Checking if {} exists, loading...".format(model_filename))
+            model = pickle.load(open(model_filename, 'rb'))
+            print("Loaded model")
+        except FileNotFoundError:
+            print("Not found, Training model to explain")
+            model.fit(X, Y)
+            print("Finished training")
+            pickle.dump(model, open(model_filename, 'wb'))
         return model
 
     # define black-box model
@@ -38,55 +46,63 @@ def custom_model(dataset_filename, X, Y):
     from keras.models import Model
     num_classes = 10
 
-    # transform to tabular
-    # x_train = x_train.reshape(x_train.shape[0], 28 * 28)
     _input = Input(shape=(X.shape[1],))
     x = Dense(512)(_input)
     x = Dense(512)(x)
     output = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=_input, outputs=output)
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # train the black-box model
-    # model.fit(X, Y, epochs=5, batch_size=2000)
-    model.fit(X, Y, epochs=5, batch_size=2000)
+    model.fit(X, Y, epochs=5, batch_size=2000)  # train the black-box model
     return ModelWrapper(model, target)
 
 
-# dataset_filename = "datasets/diabetes.csv"
+# dataset = "diabetes"
 # target = Target(target_type="classification", target_feature="class", target_value="tested_negative")
 # initial_instance_index = 0
+# cat_features = []
 
-# dataset_filename = "datasets/kc2.csv"
+# dataset = "kc2"
 # target = Target(target_type="classification", target_feature="problems", target_value="no")
 # initial_instance_index = 4
+# cat_features = []
 
-# dataset_filename = "datasets/pd_speech_features.csv"
+# dataset = "pd_speech_features"
 # target = Target(target_type="classification", target_feature="class", target_value=0)
 # initial_instance_index = 0
+# cat_features = []
 
-dataset_filename = "datasets/house_sales.csv"
-target = Target(target_type="regression", target_feature="price", target_value="increase")
+# dataset = "house_sales"
+# target = Target(target_type="regression", target_feature="price", target_value=(200000, 300000))
+# initial_instance_index = 0
+# cat_features = ["waterfront", "date_year"]
+
+dataset = "bike"
+target = Target(target_type="regression", target_feature="cnt", target_value=(1500, 2000))
 initial_instance_index = 0
-cat_features = ["waterfront", "date_year"]
+cat_features = []
 
-# dataset_filename = "datasets/mnist.csv"
+# dataset = "mnist"
 # target = Target(target_type="classification", target_feature="class", target_value=9)
 # initial_instance_index = 0
+# cat_features = []
 
 # load dataset, train model
-df = pd.read_csv(dataset_filename)
-data_analyzer = DataAnalyzer(df, target=target)
-X, Y = data_analyzer.split_dataset()
+df = pd.read_csv("datasets/" + dataset + ".csv")
+data_analyzer = DataAnalyzer(df, target=target, cat_features=cat_features)
+data_analyzer.encode()
+X, Y = data_analyzer.data()
 
 initial_instance = X[initial_instance_index]
 initial_prediction = Y[initial_instance_index]
-# np.delete(Y, initial_instance_index)
-# np.delete(X, initial_instance_index)
-model = custom_model(dataset_filename, X, Y)
+model = prepare_model(dataset, X[:10000], Y[:10000])
 
 counterfactuals, scores = bcg_xai.run(initial_instance, initial_prediction, target, data_analyzer, model)
 
-predictions = model.predict(counterfactuals)
+predictions = np.array([])
+try:
+    predictions = model.predict(counterfactuals)
+except ValueError:
+    pass
 output = {
     "initial_instance": initial_instance.tolist(),
     "initial_prediction": str(initial_prediction),
