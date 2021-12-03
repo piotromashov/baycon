53a5622abdbfe6769215f148753a04944a247443
Piotr Romashov <promachov@gmail.com>
2021-12-03 19:28:17 +0100

    Update index of initial instances to be 1:1 with MOC

--- bcgxai/run.py
+++ bcgxai/run.py
@@ -65,13 +65,13 @@ def execute(dataset, target, initial_instance_index, cat_features=[]):
     run = "0"
     models_to_run = ["RF", "SVM"]
     for model_name in models_to_run:
+        model = prepare_model(dataset, model_name, X, Y, target.target_type())
         print("--- Executing: {} Initial Instance: {} Target: {} Model: {}---".format(
             dataset,
             initial_instance_index,
             target.target_value(),
             model_name
         ))
-        model = prepare_model(dataset, model_name, X, Y, target.target_type())
         counterfactuals, scores = bcg_xai.run(initial_instance, initial_prediction, target, data_analyzer, model)
         # counterfactuals = data_analyzer.decode(counterfactuals)
 
@@ -93,129 +93,125 @@ def execute(dataset, target, initial_instance_index, cat_features=[]):
             "predictions": predictions.tolist()
         }
 
-        target_value = target.target_value()
-        target_value = "{}-{}".format(target_value[0], target_value[1]) if isinstance(target_value, tuple) else str(
-            target_value)
-        output_filename = "{}_{}_{}_{}_{}_{}.json".format("bcg", dataset, initial_instance_index, model_name,
-                                                          target_value,
-                                                          run)
+        output_filename = "{}_{}_{}_{}_{}.json".format("bcg", dataset, initial_instance_index, model_name, run)
         with open(output_filename, 'w') as outfile:
             json.dump(output, outfile)
-        print("--- Finished: saved file {}".format(output_filename))
+        print("--- Finished: saved file {}\n".format(output_filename))
 
 
 # --- BEGIN CLASSIFICATION EXPERIMENTS ---
 target = Target(target_type="classification", target_feature="class", target_value="tested_positive")
-execute("diabetes", target, initial_instance_index=0)
-execute("diabetes", target, initial_instance_index=2)
-execute("diabetes", target, initial_instance_index=4)
-execute("diabetes", target, initial_instance_index=6)
+for initial_instance_index in [1, 3, 5, 7, 10]:
+    execute("diabetes", target, initial_instance_index)
 target = Target(target_type="classification", target_feature="class", target_value="tested_negative")
-execute("diabetes", target, initial_instance_index=1)
-execute("diabetes", target, initial_instance_index=3)
-execute("diabetes", target, initial_instance_index=5)
-execute("diabetes", target, initial_instance_index=7)
-execute("diabetes", target, initial_instance_index=9)
+for initial_instance_index in [0, 2, 4, 8, 9]:
+    execute("diabetes", target, initial_instance_index)
 
 target = Target(target_type="classification", target_feature="problems", target_value="yes")
-for initial_instance_index in [8, 144, 251, 356, 412]:
+for initial_instance_index in [9, 145, 252, 357, 413]:
     execute("kc2", target, initial_instance_index)
 target = Target(target_type="classification", target_feature="problems", target_value="no")
-for initial_instance_index in [3, 420, 484, 495, 519]:
+for initial_instance_index in [4, 421, 485, 496, 520]:
     execute("kc2", target, initial_instance_index)
 
-# TODO: agregarle los features categoricos
+# df = pd.read_csv("datasets/phpGUrE90.csv")
+# for column in df.columns:
+#     print(column)
+#     print(df[column].unique())
+# # cat_features = ["V3", "V4", "V5", "V6", "V7", "V9", "V10", "V11", "V16", "V19", "V20", "V21", "V23", "V24", "V25",
+# #               "V26", "V29", "V32", "V33", "V34", "V35", "V38", "V40", "V41"]
+cat_features = []
 target = Target(target_type="classification", target_feature="Class", target_value=2)
-for initial_instance_index in [299, 510, 685, 949, 1023]:
-    execute("phpGUrE90", target, initial_instance_index)
+for initial_instance_index in [300, 511, 686, 950, 1024]:
+    execute("phpGUrE90", target, initial_instance_index, cat_features)
 target = Target(target_type="classification", target_feature="Class", target_value=1)
-for initial_instance_index in [8, 79, 144, 201, 256]:
-    execute("phpGUrE90", target, initial_instance_index)
+for initial_instance_index in [9, 80, 145, 202, 257]:
+    execute("phpGUrE90", target, initial_instance_index, cat_features)
 
+cat_features = ["gender"]
 target = Target(target_type="classification", target_feature="class", target_value=1)
 for initial_instance_index in [33, 72, 99, 195, 309]:
-    execute("pd_speech_features", target, initial_instance_index)
+    execute("parkinsonspeech", target, initial_instance_index, cat_features)
 target = Target(target_type="classification", target_feature="class", target_value=0)
 for initial_instance_index in [0, 40, 110, 230, 480]:
-    execute("pd_speech_features", target, initial_instance_index)
+    execute("parkinsonspeech", target, initial_instance_index, cat_features)
 
 # --- BEGIN REGRESSION EXPERIMENTS ---
-# TODO: what are those infinity ranges? error throwing: invalud value encountered total_diff = np.abs(max_value - min_value)
 cat_features = ["season", "yr", "mnth", "holiday", "weekday", "workingday", "weathersit"]
 target = Target(target_type="regression", target_feature="cnt", target_value=(777, 1565))
-execute("bike", target, 95, cat_features)
+execute("bike", target, 96, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(2184, 2972))
-execute("bike", target, 155, cat_features)
+execute("bike", target, 156, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(3572, 4360))
-execute("bike", target, 456, cat_features)
+execute("bike", target, 457, cat_features)
 
 target = Target(target_type="regression", target_feature="cnt", target_value=(4717, 5505))
-execute("bike", target, 95, cat_features)
+execute("bike", target, 96, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(6124, 6912))
-execute("bike", target, 155, cat_features)
+execute("bike", target, 156, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(7512, 8300))
-execute("bike", target, 456, cat_features)
+execute("bike", target, 457, cat_features)
 
 target = Target(target_type="regression", target_feature="cnt", target_value=(float("-inf"), 1565))
-execute("bike", target, 95, cat_features)
+execute("bike", target, 96, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(float("-inf"), 2972))
-execute("bike", target, 155, cat_features)
+execute("bike", target, 156, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(float("-inf"), 4360))
-execute("bike", target, 456, cat_features)
+execute("bike", target, 457, cat_features)
 
 target = Target(target_type="regression", target_feature="cnt", target_value=(4717, float("inf")))
-execute("bike", target, 95, cat_features)
+execute("bike", target, 96, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(6124, float("inf")))
-execute("bike", target, 155, cat_features)
+execute("bike", target, 156, cat_features)
 target = Target(target_type="regression", target_feature="cnt", target_value=(7512, float("inf")))
-execute("bike", target, 456, cat_features)
+execute("bike", target, 457, cat_features)
 
 cat_features = ["waterfront", "date_year"]
 target = Target(target_type="regression", target_feature="price", target_value=(80487.5, 160975.0))
-execute("housesales", target, 21064, cat_features)
+execute("housesales", target, 21065, cat_features)
 target = Target(target_type="regression", target_feature="price", target_value=(208537.5, 289025.0))
-execute("housesales", target, 47, cat_features)
+execute("housesales", target, 48, cat_features)
 target = Target(target_type="regression", target_feature="price", target_value=(403537.5, 484025.0))
-execute("housesales", target, 914, cat_features)
+execute("housesales", target, 915, cat_features)
 
 target = Target(target_type="regression", target_feature="price", target_value=(float("-inf"), 160975.0))
-execute("housesales", target, 21064, cat_features)
+execute("housesales", target, 21065, cat_features)
 target = Target(target_type="regression", target_feature="price", target_value=(float("-inf"), 289025))
-execute("housesales", target, 47, cat_features)
+execute("housesales", target, 48, cat_features)
 target = Target(target_type="regression", target_feature="price", target_value=(float("-inf"), 484025))
-execute("housesales", target, 914, cat_features)
+execute("housesales", target, 915, cat_features)
 
 target = Target(target_type="regression", target_feature="price", target_value=(482925, float("inf")))
-execute("housesales", target, 21064, cat_features)
+execute("housesales", target, 21065, cat_features)
 target = Target(target_type="regression", target_feature="price", target_value=(610975, float("inf")))
-execute("housesales", target, 47, cat_features)
+execute("housesales", target, 48, cat_features)
 target = Target(target_type="regression", target_feature="price", target_value=(805975, float("inf")))
-execute("housesales", target, 914, cat_features)
+execute("housesales", target, 915, cat_features)
 
 # TODO: add in moc jsons the target_feature
 target = Target(target_type="regression", target_feature="protein", target_value=(3.84375, 7.6625))
-execute("tecator", target, 106)
+execute("tecator", target, 107)
 target = Target(target_type="regression", target_feature="protein", target_value=(7.54375, 11.3625))
 execute("tecator", target, 86)
 target = Target(target_type="regression", target_feature="protein", target_value=(9.04375, 12.8625))
 execute("tecator", target, 2)
 
 target = Target(target_type="regression", target_feature="protein", target_value=(22.9375, 26.75625))
-execute("tecator", target, 106)
+execute("tecator", target, 107)
 target = Target(target_type="regression", target_feature="protein", target_value=(26.6375, 30.45625))
 execute("tecator", target, 86)
 target = Target(target_type="regression", target_feature="protein", target_value=(28.1375, 31.95625))
 execute("tecator", target, 2)
 
 target = Target(target_type="regression", target_feature="protein", target_value=(float("-inf"), 7.6625))
-execute("tecator", target, 106)
+execute("tecator", target, 107)
 target = Target(target_type="regression", target_feature="protein", target_value=(float("-inf"), 11.3625))
 execute("tecator", target, 86)
 target = Target(target_type="regression", target_feature="protein", target_value=(float("-inf"), 12.8625))
 execute("tecator", target, 2)
 
 target = Target(target_type="regression", target_feature="protein", target_value=(22.9375, float("inf")))
-execute("tecator", target, 106)
+execute("tecator", target, 107)
 target = Target(target_type="regression", target_feature="protein", target_value=(26.6375, float("inf")))
 execute("tecator", target, 86)
 target = Target(target_type="regression", target_feature="protein", target_value=(28.1375, float("inf")))
