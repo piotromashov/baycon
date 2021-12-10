68e2cb9ac45aa1c9fc23ccb95837ff0bc1b94775
Piotr Romashov <promachov@gmail.com>
2021-12-10 18:52:04 +0100

    Add ranker as output for testing

--- bcgxai/run.py
+++ bcgxai/run.py
@@ -86,7 +86,7 @@ def execute(dataset_name, target, initial_instance_index, categorical_features=[
                 model_name,
                 run
             ))
-            counterfactuals = bcg_xai.run(initial_instance, initial_prediction, target, data_analyzer, model)
+            counterfactuals, ranker = bcg_xai.run(initial_instance, initial_prediction, target, data_analyzer, model)
             predictions = np.array([])
             try:
                 predictions = model.predict(counterfactuals)
